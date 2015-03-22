b8e2b73875266e5ebf1ddc26cc4134c773434709
darrenladeras <darren.laderas@ninthdesign.com>
2015-03-22 16:10:13 +0800

    Updated scripts

--- js/contents.js
+++ js/contents.js
@@ -1,15 +1,73 @@
 var words = [
     {
+        name: 'Aba',
+        type: 'noun',
+        description: 'a loose sleeveless outer garment worn as traditional dress by men in the Middle East.',
+        secondDescription: 'a fabric woven from the hair of camels or goats.',
+        origin: 'Arabic',
+        firstUse: '1811'
+    }, {
+        name: 'Abaca',
+        type: 'noun',
+        description: 'a strong fiber obtained from the leafstalk of a banana (Musa textilis) native to the Philippines - called also Manila hemp.',
+        secondDescription: 'the plant that yields abaca',
+        origin: 'Spanish abaca, from Tagalog abaka.',
+        firstUse: 'circa 1805'
+    }, {
+        name: 'Abacus',
+        type: 'noun',
+        description: 'an instrument for performing calculations by sliding counters along rods or in grooves.',
+        secondDescription: 'a slab that forms the uppermost member or division of the capital pf a column.',
+        origin: 'Latin, from Greek abak-, abax, literally, slab.',
+        firstUse: '14th century'
+    }, {
         name: 'Abandon',
-        description: 'give up completely (a course of action, a practice, or a way of thinking).'
+        type: 'transitive verb',
+        description: 'to give up completely (a course of action, a practice, or a way of thinking).',
+        secondDescription: 'to withdrawn from often in the face of danger or encroachment.',
+        origin: 'Middle East',
+        firstUse: 'Arabic'
     }, {
-        name: 'Apart',
-        description: 'separated by a distance; at a specified distance from each other in time or space.'
+        name: 'Abase',
+        type: 'transitive verb',
+        description: 'to lower physically.',
+        secondDescription: 'to lower in rank, office, prestige, or esteem',
+        origin: 'Middle East',
+        firstUse: '15th century'
     }, {
-        name: 'Apartment',
-        description: 'a suite of rooms forming one residence, typically in a building containing a number of these.'
+        name: 'Abbreviation',
+        type: 'noun',
+        description: 'a shortened form of a word or name that is used in place of the full word or name.',
+        secondDescription: 'the act or result of abbreviating : abridgment',
+        origin: '',
+        firstUse: '15th century'
+    }, {
+        name: 'Abdomen',
+        type: 'noun',
+        description: 'the part of the body below the chest that contains the stomach and other organs.',
+        secondDescription: 'the rear part of an insect\'s body',
+        origin: 'Middle French and Latin',
+        firstUse: '1543'
+    }, {
+        name: 'Abuduct',
+        type: 'transitive verb',
+        description: 'to take (someone) away from a place by force.',
+        secondDescription: 'to draw or spread away (as a limb or the fingers) from a position to the median axis of the body.',
+        origin: 'Latin',
+        firstUse: '1925'
+    }, {
+        name: 'Abnormal',
+        type: 'adjective',
+        description: 'different from what is normal or average.',
+        secondDescription: 'unusual especially in a way that causes problems.',
+        origin: 'alteration of French',
+        firstUse: 'Arabic'
     }, {
         name: 'Ability',
-        description: 'possession of the means or skill to do something.'
+        type: 'noun',
+        description: 'possession of the means or skill to do something.',
+        secondDescription: 'a fabric woven from the hair of camels or goats.',
+        origin: 'Middle English',
+        firstUse: '14th century'
     }
 ];
--- js/script.js
+++ js/script.js
@@ -20,31 +20,62 @@
     // Create elements
     var b  = document.body,
         h1 = document.createElement('h1'),
-        div = document.createElement('div'),
-        ul = document.createElement('ul'),
+        cols = document.createElement('div'),
+        col1 = document.createElement('div'),
+        col2 = document.createElement('div'),
+        col3 = document.createElement('div'),
+        AtoZlist = document.createElement('ul'),
+        nameList = document.createElement('ul'),
         p = document.createElement('p');
+
+    // Set Attributes
+    cols.id = 'cols three-cols';
+    col1.className = 'col-item col-1';
+    col2.className = 'col-item col-2';
+    col3.className = 'col-item col-3';
+
+    var AtoZ = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    console.log(AtoZ);
+
+    var container = document.getElementById('container');
+    // Create a list for all the letters a-z
+    for ( var x = 0; x < AtoZ.length; x++ ) {
+        var li = document.createElement('li'),
+        a  = document.createElement('a');
+
+        a.href = '#' + AtoZ[x];
+        a.appendChild(document.createTextNode(AtoZ[x]));
+        a.setAttribute('class', 'test');
+        li.appendChild(a);
+        AtoZlist.appendChild(li);
+        col1.appendChild(AtoZlist);
+        cols.appendChild(col1);
+        container.appendChild(cols);
+    }
 
     // Create a list for all the words
     for ( var i = 0; i < words.length; i++ ) {
         var li = document.createElement('li'),
         a  = document.createElement('a');
 
-        a.href = '?id=' + i //toSlug(name);
+        a.href = '?id=' + i; //toSlug(name);
         a.appendChild(document.createTextNode(words[i].name));
         a.setAttribute('id', 'link' + ' ' + words[i].name);
         a.setAttribute('class', words[i].name);
         li.appendChild(a);
-        ul.appendChild(li);
-        div.appendChild(ul);
-        b.appendChild(div);
+        nameList.appendChild(li);
+        col2.appendChild(nameList);
+        cols.appendChild(col2);
+        container.appendChild(cols);
     }
 
-    // Remove error on index page
-    if ( typeof getUrl !== 'undefined' ) {
+    if ( typeof getUrl !== 'undefined' ) { // Remove error on index page
+        var container = document.getElementById('container');
         h1.appendChild(document.createTextNode(words[getUrl].name));
         p.appendChild(document.createTextNode(words[getUrl].description));
-        div.appendChild(h1);
-        div.appendChild(p);
-        b.appendChild(div);
+        col3.appendChild(h1);
+        col3.appendChild(p);
+        cols.appendChild(col3);
+        container.appendChild(cols);
     }
 })(window, document);
