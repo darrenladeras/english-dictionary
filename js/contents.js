bb8b71b7095ec375a780f0f98a0edc508943e5f1
Darren Laderas <darren.laderas@ninthdesign.com>
2015-05-26 10:38:03 +0800

    added additional Words

--- js/contents.js
+++ js/contents.js
@@ -70,11 +70,67 @@ var words = [
         origin: 'Middle English',
         firstUse: '14th century'
     }, {
+        name: 'Abaca',
+        type: 'noun',
+        description: 'a strong fiber obtained from the leafstalk of a banana (Musa textilis) native to the Philippines - called also Manila hemp.',
+        secondDescription: 'the plant that yields abaca',
+        origin: 'Spanish abaca, from Tagalog abaka.',
+        firstUse: 'circa 1805'
+    }, {
+        name: 'Abacus',
+        type: 'noun',
+        description: 'an instrument for performing calculations by sliding counters along rods or in grooves.',
+        secondDescription: 'a slab that forms the uppermost member or division of the capital pf a column.',
+        origin: 'Latin, from Greek abak-, abax, literally, slab.',
+        firstUse: '14th century'
+    }, {
+        name: 'Abandon',
+        type: 'transitive verb',
+        description: 'to give up completely (a course of action, a practice, or a way of thinking).',
+        secondDescription: 'to withdrawn from often in the face of danger or encroachment.',
+        origin: 'Middle East',
+        firstUse: 'Arabic'
+    }, {
+        name: 'Abase',
+        type: 'transitive verb',
+        description: 'to lower physically.',
+        secondDescription: 'to lower in rank, office, prestige, or esteem',
+        origin: 'Middle East',
+        firstUse: '15th century'
+    }, {
+        name: 'Abbreviation',
+        type: 'noun',
+        description: 'a shortened form of a word or name that is used in place of the full word or name.',
+        secondDescription: 'the act or result of abbreviating : abridgment',
+        origin: '',
+        firstUse: '15th century'
+    }, {
+        name: 'Abdomen',
+        type: 'noun',
+        description: 'the part of the body below the chest that contains the stomach and other organs.',
+        secondDescription: 'the rear part of an insect\'s body',
+        origin: 'Middle French and Latin',
+        firstUse: '1543'
+    }, {
+        name: 'Abuduct',
+        type: 'transitive verb',
+        description: 'to take (someone) away from a place by force.',
+        secondDescription: 'to draw or spread away (as a limb or the fingers) from a position to the median axis of the body.',
+        origin: 'Latin',
+        firstUse: '1925'
+    }, {
+        name: 'Abnormal',
+        type: 'adjective',
+        description: 'different from what is normal or average.',
+        secondDescription: 'unusual especially in a way that causes problems.',
+        origin: 'alteration of French',
+        firstUse: 'Arabic'
+    }, {
         name: 'Ability',
         type: 'noun',
         description: 'possession of the means or skill to do something.',
         secondDescription: 'a fabric woven from the hair of camels or goats.',
         origin: 'Middle English',
         firstUse: '14th century'
-    }
+    },
 ];
